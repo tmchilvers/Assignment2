abf311798e4e53629d75ab35ff004a7b6e4df419
MatthewParnham <parnham@chapman.edu>
2018-09-19 15:17:02 -0700

    Tested clearing screen every generation.

--- test.cpp
+++ test.cpp
@@ -3,11 +3,13 @@
 #include <chrono>
 #include <time.h>
 #include <unistd.h>
+#include <string>
 
 using namespace std;
 
 int main() {
 for(int k = 0; k < 100; k++) {
+  cout << string( 100, '\n' );
   cout << "Generation " << k+1 << endl;
   for(int i = 0; i < 10; i++){
     for(int j = 0; j < 12; j++){
