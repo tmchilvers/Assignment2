894494156c311339133f401c22b81ca36c17a0c6
Tristan Chilvers <chilvers@chapman.edu>
2018-09-20 21:21:59 +0000

    Added rough outline of grid class

--- Grid.cpp
+++ Grid.cpp
@@ -1,1 +1,72 @@
 #include "Grid.h"
+#include <string>
+
+using namespace std;
+
+Grid::Grid() //default
+{
+  //generate random cells and dimensions
+  randSetGrid(height, width);
+}
+
+Grid::Grid(int height, int width)
+{
+  //generate random cells
+  randSetGrid(height, width);
+}
+
+Grid::Grid(ifstream& mapFile)
+{
+  //read in the height and width
+  setGrid(mapFile);
+}
+
+
+//mutator methods
+void Grid::setGrid(ifstream& mapFile)
+{
+  string line;
+  for(int i = 0; i < height; i++)
+  {
+    getline(mapFile, line);
+
+    for(int j = 0; j < width, j++)
+    {
+      if(line[j] == '-')
+      {
+        cell gridboardarray[i][j].dead();
+      }
+
+      else if(line[j] == 'X' || line[j] == 'x')
+      {
+        cell gridboardarray[i][j].alive();
+      }
+
+      else
+      {
+        //catch/throw error - end program
+      }
+    }
+  }
+}
+
+void Grid::randSetGrid(int height, int width)
+{
+  int num = rand() % 2;
+
+  if(num == 0)
+  {
+    gridboardarray[i][j].dead();
+  }
+
+  else if(num == 1)
+  {
+    gridboardarray[i][j].alive();
+  }
+}
+
+
+
+
+
+//note: may need copy constrcutor for shadow thing
---
+++ Grid.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Grid
+{
+
+  public:
+    //constructors
+    Grid(); //default
+    Grid(int height, int width); //random cells only
+    Grid(ifstream& mapFile); //file input
+
+    //Destructor
+    ~Grid();
+
+    //Mutator functions
+    void setGrid(ifstream& mapFile);
+    void randSetGrid(int height, int width);
+}
