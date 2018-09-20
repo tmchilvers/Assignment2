786607c991b117d36ea378e8ae5e84c97d7d1ca3
MatthewParnham <parnham@chapman.edu>
2018-09-20 14:32:42 -0700

    test.

--- test.cpp
+++ test.cpp
@@ -11,9 +11,17 @@ int main() {
 for(int k = 0; k < 100; k++) {
   cout << string( 100, '\n' );
   cout << "Generation " << k+1 << endl;
-  for(int i = 0; i < 10; i++){
-    for(int j = 0; j < 12; j++){
+  for(int i = 0; i < 30; i++){
+    for(int j = 0; j < 20; j++){
       if (i==7 && j==4 && k%2==0) {
+        cout << "x ";
+        continue;
+      }
+      if (i==18 && j==13 && k%3==0) {
+        cout << "x ";
+        continue;
+      }
+      if (i==24 && j==9 && k%5==0) {
         cout << "x ";
         continue;
       }
