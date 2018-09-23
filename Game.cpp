94daddb203cda891083f5bdfab7f4a903665ac34
MatthewParnham <parnham@chapman.edu>
2018-09-22 17:15:47 -0700

    Added #includes to Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,3 +1,6 @@
+#include "Game.h"
+#include <iostream>
+
 //Constructors
 Game::Game()
 {
--- test.cpp
+++ test.cpp
@@ -10,31 +10,7 @@
 using namespace std;
 
 int main() {
-/*for(int k = 0; k < 100; k++) {
-  cout << string( 100, '\n' );
-  cout << "Generation " << k+1 << endl;
-  for(int i = 0; i < 30; i++){
-    for(int j = 0; j < 20; j++){
-      if (i==7 && j==4 && k%2==0) {
-        cout << "x ";
-        continue;
-      }
-      if (i==18 && j==13 && k%3==0) {
-        cout << "x ";
-        continue;
-      }
-      if (i==24 && j==9 && k%5==0) {
-        cout << "x ";
-        continue;
-      }
-      cout << "- ";
 
-    }
-    cout << "\n";
-
-  }
-  sleep(1);
-}*/
   Grid grid1(40,60);
   grid1.printGrid();
   cout << "\n";
