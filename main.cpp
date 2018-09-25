05f78c5e9de23331426a2819e0d54455f1c78faf
Tristan Chilvers <chilvers@chapman.edu>
2018-09-25 14:06:01 -0700

    Changed test.cpp to main.cpp and updated the file to look clean

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <time.h>
+#include <unistd.h>
+#include <string>
+#include "Grid.h"
+#include "GameMode.h"
+#include "Game.h"
+
+using namespace std;
+
+int main() {
+  Game game; //this calls the game class constructor to run the Game of Life program!!
+
+  return 0;
+}
